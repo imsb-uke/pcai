3248a737ad4e312e474eaf0f56fddd294d3fc468
Fabian Westhäußer <fabian.westhaeusser@zmnh.uni-hamburg.de>
2024-05-06 11:00:38 +0200

    adversarial net pt 2

---
+++ src/architecture/__init__.py
@@ -0,0 +1,1 @@
+from .adversarial_net import AdversarialNet
--- src/architecture/components/__init__.py
+++ src/architecture/components/__init__.py
@@ -2,7 +2,5 @@ from .adv_module import AdversarialModule
 from .aggregation_module import BagAggregationModule
 from .encoder_module import BagCnnEncoderModule
 from .self_attention_module import BagSelfAttentionModule
-from .target_module import (
-    BagClassificationTargetModule,
-    ClassificationTargetModule,
-)
+from .target_module import ClassificationTargetModule
+
