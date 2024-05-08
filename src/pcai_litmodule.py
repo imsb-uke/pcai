2abd989047a0bd0fc32d66e83d5fe4f1676ac4fa
Fabian Westhäußer <fabian.westhaeusser@zmnh.uni-hamburg.de>
2024-05-08 16:09:18 +0200

    add metrics

--- src/pcai_litmodule.py
+++ src/pcai_litmodule.py
@@ -8,6 +8,11 @@ from pytorch_lightning import LightningModule
 
 from src.architecture import AdversarialNet
 
+from torchmetrics import MaxMetric, MeanMetric
+from torchmetrics.classification import (
+    MulticlassAUROC,
+)
+
 
 class LitModuleClasAdversarial(LightningModule):
     """A LightningModule organizes your PyTorch code into 6 sections:
@@ -42,6 +47,19 @@ class LitModuleClasAdversarial(LightningModule):
             self.criterion_clas = torch.nn.CrossEntropyLoss()
             self.criterion_adv = torch.nn.CrossEntropyLoss()
 
+            # build metric objects
+            self.metrics = torch.nn.ModuleDict()
+            for mode in ["train", "val", "test"]:
+                self.metrics[f"{mode}_loss"] = MeanMetric()
+                self.metrics[f"{mode}_loss_clas"] = MeanMetric()
+                self.metrics[f"{mode}_loss_adv"] = MeanMetric()
+                self.metrics[f"{mode}_auroc_clas"] = MulticlassAUROC(
+                    num_classes=2, average="weighted"
+                )
+                self.metrics[f"{mode}_auroc_adv"] = MulticlassAUROC(
+                    num_classes=3, average="weighted"
+                )
+                
 
     def forward(self, x: torch.Tensor):
         return self.net(x)
@@ -53,6 +71,18 @@ class LitModuleClasAdversarial(LightningModule):
         probs = torch.softmax(x_hat, dim=1)
 
         return loss, preds, probs
+    
+    def _log_metrics(self, mode: str, head: str, loss, probs, targets):
+        self.metrics[f"{mode}_loss_{head}"](loss)
+        self.metrics[f"{mode}_auroc_{head}"](probs, targets)
+        for metric in ["loss", "auroc",]:
+            self.log(
+                f"{mode}/{metric}_{head}",
+                self.metrics[f"{mode}_{metric}_{head}"],
+                on_step=False,
+                on_epoch=True,
+                prog_bar=True,
+            )
 
     def step(self, batch: Any, mode: str):
         x, y_clas, y_adv, data_meta = batch
@@ -60,20 +90,34 @@ class LitModuleClasAdversarial(LightningModule):
         x_hat_clas, x_hat_adv = self.forward(x)
 
         # clas
-        loss_bin_clas, preds_bin_clas, probs_bin_clas = self._step(x_hat_clas, y_clas, self.criterion_clas)
+        loss_clas, preds_clas, probs_clas = self._step(x_hat_clas, y_clas, self.criterion_clas)
 
+        self._log_metrics(mode, "clas", loss_clas, probs_clas, y_clas)
+        
         # adv
         loss_adv, preds_adv, probs_adv = self._step(x_hat_adv, y_adv, self.criterion_adv)
 
-        total_loss = loss_bin_clas + self.hparams.lambda_adv * loss_adv
+        self._log_metrics(mode, "adv", loss_adv, probs_adv, y_adv)
+
+        # combined
+        total_loss = loss_clas + self.hparams.lambda_adv * loss_adv
+
+        self.metrics[f"{mode}_loss"](total_loss)
+        self.log(
+            f"{mode}/loss",
+            self.metrics[f"{mode}_loss"],
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+        )
 
         out_dict = {
             "loss": total_loss,
-            "loss_bin_clas": loss_bin_clas,
-            "logits_bin_clas": x_hat_clas,
-            "targets_bin_clas": y_clas,
-            "preds_bin_clas": preds_bin_clas,
-            "probs_bin_clas": probs_bin_clas,
+            "loss_clas": loss_clas,
+            "logits_clas": x_hat_clas,
+            "targets_clas": y_clas,
+            "preds_clas": preds_clas,
+            "probs_clas": probs_clas,
         }
         out_dict |= {
             "loss_adv": loss_adv,
@@ -104,7 +148,4 @@ class LitModuleClasAdversarial(LightningModule):
 
     def configure_optimizers(self):
         optimizer = Adam(params=self.parameters(), lr=self.hparams.lr)
-
-        out_dict = {"optimizer": optimizer}
-
-        return out_dict
+        return {"optimizer": optimizer}
